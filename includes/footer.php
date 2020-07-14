189b64bcb729bb34aa47db628812f42a624e0428
ravi5567 <35301013+ravi5567@users.noreply.github.com>
2020-07-14 21:03:58 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+
+
+
+
+
+<!---
+<div class="copyright" style="position:absolute;bottom:;">
+	<div class="container">
+	
+		<div class="footer-social-icons wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+		 <a >												Ravindra Gupta 					<a/>
+	</div>
+</div>
+-->
